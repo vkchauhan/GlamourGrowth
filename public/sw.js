8219c3ee8f3562b43540b2be803587009566317e
vkchauhan <v.chauhan144@gmail.com>
2026-03-08 13:01:45 +0530

    feat: Add service worker for offline support
    Implement a service worker to cache essential application assets. This change enables offline functionality and improves application load times by serving cached resources when the network is unavailable.

---
+++ public/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'glamour-growth-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-256.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
